cfb7533879e6624736d0746758006963d8b748a5
adkhambek <mr.adkhambek@gmail.com>
2020-10-24 22:46:13 +0500

    🤫

---
+++ app/src/main/java/mr/adkhambek/mvvm/utils/InjectorX.kt
@@ -0,0 +1,44 @@
+package mr.adkhambek.mvvm.utils
+
+import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.google.gson.Gson
+import mr.adkhambek.mvvm.di.ApiModule
+import mr.adkhambek.mvvm.di.NetworkModule
+import mr.adkhambek.mvvm.network.MainAPI
+import mr.adkhambek.mvvm.repository.MainRepository
+import mr.adkhambek.mvvm.usecase.load.LoadProductsUseCase
+import mr.adkhambek.mvvm.usecase.load.LoadProductsUseCaseImpl
+import mr.adkhambek.mvvm.usecase.sell.SellUseCase
+import mr.adkhambek.mvvm.usecase.sell.SellUseCaseImpl
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+
+inline fun <reified C : Context> C.getMainAPI(): MainAPI {
+    val chuckCollector: ChuckerCollector = NetworkModule.provideChuckerCollector(this)
+    val chuck: ChuckerInterceptor = NetworkModule.provideChuckerInterceptor(this, chuckCollector)
+
+    val httpLoggingInterceptor: HttpLoggingInterceptor = NetworkModule.provideHttpLoggingInterceptor()
+
+    val gson: Gson = NetworkModule.provideGson()
+    val okHttpClient: OkHttpClient = NetworkModule.provideClient(chuck, httpLoggingInterceptor)
+
+    val retrofit: Retrofit = NetworkModule.provideRetrofit(gson, okHttpClient)
+
+    return ApiModule.provideMainAPI(retrofit)
+}
+
+fun getMainRepository(mainAPI: MainAPI): MainRepository {
+    return MainRepository(mainAPI)
+}
+
+fun getSellUseCase(mainRepository: MainRepository): SellUseCase {
+    return SellUseCaseImpl(mainRepository)
+}
+
+fun getLoadProductsUseCase(mainRepository: MainRepository): LoadProductsUseCase {
+    return LoadProductsUseCaseImpl(mainRepository)
+}
